08f0c58ef6b4b7479734b764443ac607cdde53e0
Anna Dybas <a.m.dybas@gmail.com>
2019-05-24 00:17:33 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  my_life = {"name" => "Ania", "age" => 30, "occupation" => "Front End Web Developer"}
   
 end
 
